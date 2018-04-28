11edb3574df6409b3d4c82f5b80e52826da7a58f
Ícaro Magalhães <icaro.lmag@gmail.com>
2018-04-27 23:55:20 -0300

    Add histogram normalization operation

--- assignment-02/src/main.py
+++ assignment-02/src/main.py
@@ -1,6 +1,8 @@
 from PIL import Image, ImageOps
 import numpy as np
 import itertools
+import functools
+import operator
 
 class DetailedImage(object):
     def __init__(self, raw_content):
@@ -13,12 +15,15 @@ class DetailedImage(object):
     def meta_copy(self):
         return self.raw_image_copy(), itertools.product(range(self.rows), range(self.columns))
 
+
 def load_image(path):
     raw_image = Image.open(path)
     return DetailedImage(raw_content=raw_image)
 
+
 def show_image(detailed_image):
     detailed_image.raw_content.show()
+
 
 def rgb_to_y_band(image):
     image, image_range = image.meta_copy()
@@ -31,7 +36,8 @@ def rgb_to_y_band(image):
 
     return DetailedImage(image)
 
-def histogram_expand(image, border=0, fill=0):
+
+def histogram_expansion(image):
     def gxy(fxy, min, max):
         return int(((fxy - min) * 255)/(max-min))
 
@@ -49,15 +55,51 @@ def histogram_expand(image, border=0, fill=0):
     return DetailedImage(image)
 
 
+def histogram_equalization(image):
+    """
+    Create a uniform distribution of grayscale values in
+    the output image by applying a non linear mapping based on
+    Pillow's histogram equalization algorithm [IM]
+    """
+    image, image_range = image.meta_copy()
+    pixels = image.load()
+    range_256 = range(256)
+    original_histogram = image.histogram()
+    lookup_table = []
+    for band_idx in range(0, len(original_histogram), 256):
+        histo = [_f for _f in original_histogram[band_idx:band_idx+256] if _f]
+        if len(histo) <= 1:
+            lookup_table.extend(list(range_256))
+        else:
+            step = (functools.reduce(operator.add, histo) - histo[-1]) // 255
+            if not step:
+                lookup_table.extend(list(range_256))
+            else:
+                n = step // 2
+                for i in range_256:
+                    lookup_table.append(n // step)
+                    n = n + original_histogram[i+band_idx]
+    return DetailedImage(image.point(lookup_table))
+
+
 def simulate(image):
     show_image(image)
     
     y_band_image = rgb_to_y_band(image)
     show_image(y_band_image)
 
-    expanded = histogram_expand(y_band_image)
+    expanded = histogram_expansion(y_band_image)
     show_image(expanded)
-    
+
+    equalized = histogram_equalization(y_band_image)
+    show_image(equalized)
+
+    equalized_and_expanded = histogram_expansion(equalized)
+    expanded_and_equalized = histogram_equalization(expanded)
+
+    show_image(equalized_and_expanded)
+    show_image(expanded_and_equalized)
+
 
 def main():
     LENA_IMAGE_PATH = "../assets/images/lena.jpg"
